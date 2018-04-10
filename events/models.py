529cc78901b040eb9ed8ada8df11f76b713c7275
dz.luke <dz.luke@gmail.com>
2018-04-09 18:06:43 -0700

    first commit

---
+++ events/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+from django.urls import reverse
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class Event(models.Model):
+    day = models.DateField(u'Day of the event', help_text=u'Day of the event')
+    start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
+    end_time = models.TimeField(u'Final time', help_text=u'Final time')
+    notes = models.TextField(u'Textual Notes', help_text=u'Textual Notes', blank=True, null=True)
+
+    class Meta:
+        verbose_name = u'Scheduling'
+        verbose_name_plural = u'Scheduling'
+
+    def get_absolute_url(self):
+        url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
+        return u'<a href="%s">%s</a>' % (url, str(self.start_time))
+
+    def clean(self):
+        if self.end_time <= self.start_time:
+            raise ValidationError('Ending times must after starting times')
+
+
+# Create your models here.
